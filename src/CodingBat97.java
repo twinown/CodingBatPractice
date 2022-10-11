8c8943a301de3e09419e99a29d82304f072ad5e9
twinown <niyaz9797@gmail.com>
2022-10-11 20:37:55 +0200

    95-97

---
+++ src/CodingBat97.java
@@ -0,0 +1,14 @@
+/*Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat,
+        and a boolean indicating if we are on vacation, return a string of the form "7:00" i
+        ndicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and
+        on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it
+        should be "10:00" and weekends it should be "off".
+        alarmClock(1, false) → "7:00"
+        alarmClock(5, false) → "7:00"
+        alarmClock(0, false) → "10:00"*/
+public class CodingBat97 {
+    public String alarmClock(int day, boolean vacation) {
+        return   vacation && (day==6||day==0) ? "off":
+                vacation || (day==6)||(day==0) ? "10:00": "7:00";
+    }
+}
