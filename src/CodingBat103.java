82baa65e09655b4a5a62d70b9e96cf86631db166
twinown <niyaz9797@gmail.com>
2022-10-13 18:04:40 +0200

    101-103

---
+++ src/CodingBat103.java
@@ -0,0 +1,11 @@
+/*
+Return true if the given non-negative number is 1 or 2 less than a multiple of 20.
+        So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+        less20(18) → true
+        less20(19) → true
+        less20(20) → false*/
+public class CodingBat103 {
+    public boolean less20(int n) {
+        return (n+1)%20==0||(n+2)%20==0;
+    }
+}
