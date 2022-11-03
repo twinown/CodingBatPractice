c38ae2c0f98bb678f9c00204d1a54dc51eefb282
twinown <niyaz9797@gmail.com>
2022-11-04 00:56:17 +0300

    144-147

--- src/CodingBat144.java
+++ src/CodingBat144.java
@@ -1,2 +1,15 @@
+/*
+We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the
+        bunnies recursively (without loops or multiplication).
+        bunnyEars(0) → 0
+        bunnyEars(1) → 2
+        bunnyEars(2) → 4*/
 public class CodingBat144 {
+    public int bunnyEars(int bunnies) {
+        int result;
+        if(bunnies == 0){
+            return 0;
+        }
+        return bunnyEars(bunnies-1)+2;
+    }
 }
