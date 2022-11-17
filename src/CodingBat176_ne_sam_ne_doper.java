14cb70accf7b4ac49bc64cb9240283b0511483d5
twinown <niyaz9797@gmail.com>
2022-11-18 02:10:06 +0300

    174-176

---
+++ src/CodingBat176_ne_sam_ne_doper.java
@@ -0,0 +1,7 @@
+public class CodingBat176_ne_sam_ne_doper {
+    public boolean closeFar(int a, int b, int c) {
+        return Math.abs(b-a)<=1&&Math.abs(c-a)>=2&&Math.abs(c-b)>=2||
+                Math.abs(c-a)<=1&&Math.abs(b-a)>=2&&Math.abs(b-c)>=2;
+    }
+
+}
