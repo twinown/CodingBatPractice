9c1d88ff45e67df6e818aa0d95ae1094190e8a97
twinown <niyaz9797@gmail.com>
2022-11-19 03:15:50 +0300

    177-179

--- src/CodingBat171_ne_sam_trudno.java
+++ src/CodingBat171_ne_sam_trudno.java
@@ -6,6 +6,7 @@
         makeBricks(3, 2, 10) → true*/
 public class CodingBat171_ne_sam_trudno {
     public boolean makeBricks(int small, int big, int goal) {
+
         return small+(big*5)>=goal&&goal%5<=small;
         }
 }
