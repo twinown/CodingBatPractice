d6ab625b47b2dfd8226bbaa13da9d3ec0b5dd748
twinown <niyaz9797@gmail.com>
2022-12-03 01:46:25 +0300

    197

--- src/CodingBat197_imp_for_deleting_chars.java
+++ src/CodingBat197_imp_for_deleting_chars.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /*
 Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and
         ending with 'p'. Return a string where for all such words, the middle letter is
@@ -7,15 +5,18 @@ Look for patterns like "zip" and "zap" in the string -- length-3, starting with 
         zipZap("zipXzap") → "zpXzp"
         zipZap("zopzop") → "zpzp"
         zipZap("zzzopzop") → "zzzpzp"*/
-public class CodingBat197 {
+public class CodingBat197_imp_for_deleting_chars {
     public static String zipZap(String str) {
         for(int i = 0; i<=str.length()-3;i++){
             if(str.charAt(i)=='z'&&str.charAt(i+2)=='p'){
-                str = str.replace(str.substring(i+1,i+2),"");
+                str = str.substring(0,i+1)+str.substring(i+2);
             }
         }
         return str;
     }
 
+    public static void main(String[] args) {
+        System.out.println(zipZap("zipXzap"));
+    }
 
 }
