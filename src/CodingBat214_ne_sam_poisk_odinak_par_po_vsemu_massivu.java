ca6b5fc6f7be61875b01961ca6d5a96426b0aeda
twinown <niyaz9797@gmail.com>
2022-12-19 01:04:31 +0300

    216-218

--- src/CodingBat214_ne_sam_poisk_odinak_par_po_vsemu_massivu.java
+++ src/CodingBat214_ne_sam_poisk_odinak_par_po_vsemu_massivu.java
@@ -6,7 +6,7 @@ We'll say that a value is "everywhere" in an array if for every
         isEverywhere([1, 2, 1, 3], 1) → true
         isEverywhere([1, 2, 1, 3], 2) → false
         isEverywhere([1, 2, 1, 3, 4], 1) → false*/
-public class CodingBat214_ne_sam_poisk_odinak_par {
+public class CodingBat214_ne_sam_poisk_odinak_par_po_vsemu_massivu {
     public static boolean isEverywhere(int[] nums, int val) {
             boolean b = true;
             for (int i = 0; i<nums.length-2;i++){
--- src/CodingBat215.java
+++ src/CodingBat215.java
@@ -6,26 +6,30 @@ Given an array of ints, return true if the array contains a
         either24([4, 4, 1, 2, 2]) → false*/
 public class CodingBat215 {
     public static boolean either24(int[] nums) {
-        int counta = 0;
-        int countb = 0;
         boolean b = false;
-        for (int i = 1; i < nums.length;i++){
+        int counttwo = 0;
+        int countfour = 0;
+        for (int i = 0; i < nums.length;i++){
             if(nums[i]==2){
-                counta++;
-            } else if (nums[i]==4)
-               {
-                   countb++;
-               }
-                if(nums[i]==nums[i-1]){
-                    b = true;
-                }
+                counttwo++;
+            } else if(nums[i]==4){
+                countfour++;
             }
-
-        if(counta==2&&countb==2){
+        }
+        for (int i = 0; i < nums.length-1;i++){
+            if ((nums[i] == 2 && nums[i + 1] == nums[i]) ||
+                    (nums[i] == 4 && nums[i + 1] == nums[i])) {
+                b = true;
+                break;
+            }
+        }
+        if(counttwo>=2&&countfour>=2){
             b = false;
         }
         return b;
     }
+
+
 
     public static void main(String[] args) {
         System.out.println(either24(new int[]{4, 4, 1, 2, 2}));
