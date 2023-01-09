fd4b9e3bc3de6f24c15d3e6fb5880414fdaf70e4
twinown <niyaz9797@gmail.com>
2023-01-10 01:10:17 +0300

    240

--- src/CodingBat240.java
+++ src/CodingBat240.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Loop over the given array of strings to build a result string like this: when
         a string appears the 2nd, 4th, 6th, etc. time in the array, append
@@ -6,7 +9,18 @@ Loop over the given array of strings to build a result string like this: when
         wordAppend(["a", "b", "a", "c", "a", "d", "a"]) → "aa"
         wordAppend(["a", "", "a"]) → "a"*/
 public class CodingBat240 {
-    public static void main(String[] args) {
+    public static Map<String, Integer> wordAppend(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        int count = 0;
+        String str = "";
+            for (int i = 0;i<strings.length;i++) {
+              map.put(strings[i],i);
+            }
+        return map;
+    }
 
+
+    public static void main(String[] args) {
+        System.out.println(wordAppend(new String[]{"a", "b", "b", "b", "a", "c", "a", "a", "a", "b", "a"}));
     }
 }
