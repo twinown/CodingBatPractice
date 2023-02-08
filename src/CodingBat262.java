dcac0350890933df368b26107210ec8dfddfc1ac
twinown <niyaz9797@gmail.com>
2023-02-08 20:16:24 +0300

    262

---
+++ src/CodingBat262.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/*
+We'll say that a "mirror" section in an array is a group of contiguous elements such that
+        somewhere in the array, the same group appears in reverse order. For example,
+        the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+        Return the size of the largest mirror section found in the given array.
+        maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
+        maxMirror([1, 2, 1, 4]) → 3
+        maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2*/
+public class CodingBat262 {
+    public static int maxMirror(int[] nums) {
+       int length = 0;
+        for (int i = 0; i <  ; i++) {
+            for (int j = 0; j < ; j++) {
+
+            }
+        }
+        // TODO: 08.02.2023 дальше думать
+        return length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxMirror(new int[]{7, 1, 2, 9, 7, 2, 1}));
+    }
+}
