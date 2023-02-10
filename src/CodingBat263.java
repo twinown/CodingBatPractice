e0936d015ad350b98a2997e74b9e3561016ea3a6
twinown <niyaz9797@gmail.com>
2023-02-10 17:23:30 +0300

    236

--- src/CodingBat263.java
+++ src/CodingBat263.java
@@ -2,7 +2,31 @@ import java.util.Arrays;
 
 public class CodingBat263 {
 
-
+    public static int countClumps(int[] nums) {
+        int n;
+        int count = 0;
+        int last = 0;
+        for (int i = 0; i < nums.length; i++) {
+            n = 1;
+            for (int j = i + 1; j <= nums.length - 1; j++) {
+                if (nums[i] == nums[j]) {
+                    n++;
+                    last = j;
+                } else if (nums[i] != nums[j]) {
+                    break;
+                }
+            }
+            if (n >= 2) {
+                count++;
+                i = last;
+            }
+        }
+        return count;
     }
+    
+    public static void main(String[] args) {
+        System.out.println(countClumps(new int[]{0, 0, 0, 2, 2, 1, 1, 1, 2, 1, 1, 2, 2}));
+    }
+}
 
 
