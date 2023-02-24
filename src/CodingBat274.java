fb443232ade7c49609748cd78efb745fe04dc2a1
twinown <niyaz9797@gmail.com>
2023-02-24 21:50:35 +0300

    274

--- src/CodingBat274.java
+++ src/CodingBat274.java
@@ -9,15 +9,18 @@
         array6([6], 0) → true*/
 public class CodingBat274 {
     public static boolean array6(int[] nums, int index) {
-        // TODO: 23.02.2023 думай
-        boolean b;
-        if (nums[index] == 6) {
-            b = true;
-        } else b = false;
-        return array6(nums, index + 1);
+        boolean b = false;
+        if (nums.length!=0) {
+            if (nums[index] == 6) {
+                return true;
+            } else if (index + 1 < nums.length) {
+                return array6(nums, index + 1);
+            }
+        }
+            return b;
     }
 
     public static void main(String[] args) {
-        System.out.println(array6(new int[]{1, 6, 4}, 0));
+        System.out.println(array6(new int[]{}, 0));
     }
 }
