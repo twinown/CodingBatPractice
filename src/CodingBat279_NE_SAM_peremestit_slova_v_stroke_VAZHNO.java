2bab58c317b81c61698d36cac8e405a3885c1b4c
twinown <niyaz9797@gmail.com>
2023-03-02 19:00:01 +0300

    279

---
+++ src/CodingBat279_NE_SAM_peremestit_slova_v_stroke_VAZHNO.java
@@ -0,0 +1,11 @@
+public class CodingBat279_NE_SAM_peremestit_slova_v_stroke_VAZHNO {
+    public static String endX(String str) {
+        if (str.equals("")) return str;
+        if (str.charAt(0) == 'x') return endX(str.substring(1)) + 'x';
+        else return str.charAt(0) + endX(str.substring(1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(endX("xxre"));
+    }
+}
