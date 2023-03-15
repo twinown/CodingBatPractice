e31538b17ca6461fdd7c2b06b75635137f2f6ebd
twinown <niyaz9797@gmail.com>
2023-03-15 10:54:36 +0300

    290

---
+++ src/CodingBat290_lambdas_in_list.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CodingBat290_lambdas_in_list {
+    public static List<Integer> doubling(List<Integer> nums) {
+        return nums.stream().map(n->n*2).collect(Collectors.toList());
+
+        //or
+       /* nums.replaceAll(n->n*2);
+        return nums;*/
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doubling(Arrays.asList(1,2,3)));
+    }
+}
