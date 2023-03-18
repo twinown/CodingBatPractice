51d41baaad7f8a41b937688c3b46ee0fe1d63029
twinown <niyaz9797@gmail.com>
2023-03-18 15:10:57 +0300

    293

---
+++ src/CodingBat293.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class CodingBat293 {
+    public List<String> copies3(List<String> strings) {
+        strings.replaceAll(n->n+n+n);
+        return strings;
+    }
+}
