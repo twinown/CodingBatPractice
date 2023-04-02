6b0f48f85bca54b1e4c7e31585ba9c1a2d74f526
twinown <niyaz9797@gmail.com>
2023-04-02 22:59:20 +0300

    262_COULD_NOT_:(

--- src/CodingBat262_NE_SMOG_OCHEN_TRUDNAYA_MIRROR.java
+++ src/CodingBat262_NE_SMOG_OCHEN_TRUDNAYA_MIRROR.java
@@ -1,6 +1,8 @@
 import javax.management.MBeanRegistration;
 import java.lang.ref.SoftReference;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -12,116 +14,38 @@ We'll say that a "mirror" section in an array is a group of contiguous elements 
         maxMirror([1, 2, 1, 4]) → 3
         maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2*/
 public class CodingBat262_NE_SMOG_OCHEN_TRUDNAYA_MIRROR {
-
-
     public static int maxMirror(int[] nums) {
-        int count = 0;
-        List<Integer> stringsLength = new ArrayList<>();
-        for (int i = 0; i <nums.length  ; i++) {
-            for (int j = 1; j <nums.length ; j++) {
-                if (nums[j]>nums[j-1]){
-                    int temp  = nums[j-1];
-                    nums[j-1]  =nums[j];
+List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = 1; j < nums.length ; j++) {
+                if (nums[j]<nums[j-1]){
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
                     nums[j] = temp;
                 }
             }
         }
-        for (int i = 0; i < nums.length-1; i++) {
+        for (int i = 0; i < nums.length-1 ; i++) {
             if (nums[i]==nums[i+1]){
-                stringsLength.add(nums[i]);
-
+                list.add(nums[i]);
+                list.add(nums[i+1]);
+                i++;
             }
         }
-        /*String prevS = "";
-        int max = 0;
-        int length = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-       //     prevS = "";
-            for (int j = length - 1; j >= 0; j--) {
-                if (nums[i] == nums[j]) {
-                    i++;
-                    prevS+=nums[i];
-                } else
-            }
-
-        }
-        stringsLength.add(prevS.length());
-
-        max = stringsLength.get(0);
-        for (Integer i : stringsLength
-        ) {
-            if (i > max) {
-                max = i;
+        System.out.println(list);
+        for (int i = 0; i <list.size()-2; i=i+2) {
+            if (list.get(i)==list.get(i+2)){
+                list.remove(i);
             }
         }
-        return max;*/
-        return stringsLength.size();
+        System.out.println(list);
+        return list.size();
     }
 
 
 
-
-
-
-
-    /*   int length = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = nums.length-1; j>i; j--) {
-                if (nums[i] == nums[j]){
-                    if (Math.abs(nums[i]-nums[i+1])==Math.abs(nums[j]-nums[j-1])){
-                   length++;
-                    }
-                }
-            }
-        }
-        return length;*/
-       /* List <Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (Math.abs(nums[i] - nums[i + 1]) == 1) {
-                for (int j = nums.length - 1; j > i; j--) {
-                    if (nums[j] == nums[i]) {
-                        if ((Math.abs(nums[j] - nums[j - 1]) == 1)||Math.abs(nums[j] - nums[j + 1])==1) {
-                            //if (!list.contains(nums[i])) {
-                                list.add(nums[i]);
-                          //  }
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return list;*/
-    /*    List <Integer> list = new ArrayList<>();
-        int inumb = 0;
-        int fast = 0;
-     //   int n = nums.length-1;
-        for (int i = 0; i < nums.length ; i++) {
-            fast = 0;
-            inumb = i;
-            for (int j = nums.length-1; j > i; j--) {
-                if (nums[i] == nums[j]){
-                    for (int k = j; k >i ; k--) {
-                        if (nums[i]==nums[k]){
-                            list.add(nums[i]);
-                            fast++;
-                            i++;
-                        } else break;
-                    }
-                    if (fast<2){
-                        i = inumb;
-                        list.remove(i);
-
-                    }
-                }
-            }
-
-        }
-        return list;*/
-    // }
-
     public static void main(String[] args) {
         System.out.println(maxMirror(new int[]{5, 9, 9, 4, 5, 4, 9, 9, 2}));       //3
-        //     System.out.println(maxMirror(new int[]{1, 2, 1, 4}));                   //3
-        //  System.out.println(maxMirror(new int[]{7, 1, 2, 9, 7, 2, 1}));          //2
+      //2
     }
 }
