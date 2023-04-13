5432a4bb498a9b96ab341ad12878d2570e7239e6
twinown <niyaz9797@gmail.com>
2023-04-13 15:59:05 +0300

    309

---
+++ src/CodingBat309.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class CodingBat309 {
+    public List<Integer> square56(List<Integer> nums) {
+        nums.replaceAll(n->n*n+10);
+        nums.removeIf(n->n%10==5||n%10==6);
+        return nums;
+    }
+}
