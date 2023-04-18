068888a472f61e6fc6067dbb8112c97bec341660
twinown <niyaz9797@gmail.com>
2023-04-18 17:33:39 +0300

    142

--- src/CodingBat142_POISK_ODINAK_BUKV_V_2_MASSIVAH.java
+++ src/CodingBat142_POISK_ODINAK_BUKV_V_2_MASSIVAH.java
@@ -6,7 +6,9 @@ Start with two arrays of strings, a and b, each in alphabetical order, possibly 
                 commonTwo(["a", "c", "x"], ["b", "c", "d", "x"]) → 2
                 commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) → 3
                 commonTwo(["a", "b", "c"], ["a", "b", "c"]) → 3*/
-public class CodingBat142_NE_SAM_POISK_ODINAK_BUKV_V_2_MASSIVAH {
+public class CodingBat142_POISK_ODINAK_BUKV_V_2_MASSIVAH {
+
+
     public static int commonTwo(String[] a, String[] b) {
         //здесь не нужно искать минимум , либо максимум , чтоб знать, по какому массиву идти
         //здесь фишка проверки на дубликат в том, что кидаешь пройденные символы в string и
