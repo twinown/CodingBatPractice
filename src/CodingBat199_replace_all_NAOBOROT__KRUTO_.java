ebebf8b25d93fbd073a175421741b056fd7cf21f
twinown <niyaz9797@gmail.com>
2023-04-24 15:19:12 +0300

    199

--- src/CodingBat199_replace_all_NAOBOROT__KRUTO_.java
+++ src/CodingBat199_replace_all_NAOBOROT__KRUTO_.java
@@ -5,13 +5,10 @@ Given a string and a non-empty word string, return a version of the original Str
         plusOut("12xy34", "xy") → "++xy++"
         plusOut("12xy34", "1") → "1+++++"
         plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"*/
-public class CodingBat199_NE_SAM_replace_all_NAOBOROT__KRUTO_ {
+public class CodingBat199_replace_all_NAOBOROT__KRUTO_ {
     public static String plusOut(String str, String word) {
-        str = str.replace(word, "*");
-        str = str.replaceAll("[^\\*]", "+");
-        return  str.replace("*", word);
+        return str.replace(word,"*").replaceAll("[^*]","+").replace("*", word);
     }
-
     public static void main(String[] args) {
         System.out.println(plusOut("12xy34xyabcxy", "xy"));
     }
